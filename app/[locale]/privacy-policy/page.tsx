11846e0498e23547e11ff519a6f14e396112b12d
WasiakKacper <wasiak.kacper11@gmail.com>
2026-04-29 23:08:01 +0200

    feat(PrivacyPolicy.tsx, pl.json, en.json): Add translation for Privacy Policy.

--- app/[locale]/privacy-policy/page.tsx
+++ app/[locale]/privacy-policy/page.tsx
@@ -1,21 +1,29 @@
 import Header from "../../../components/Ui/Header/Header";
 import Footer from "../../../components/Ui/Footer/Footer";
-import TextContent from "./textContent";
 import Paragraph from "../../../components/Ui/Paragraph/Paragraph";
+import { useTranslations } from "next-intl";
 
+type PolicySections = {
+  heading: string,
+  content: string,
+}
 
 const PrivacyPolicy = () => {
+
+  const t = useTranslations('PrivacyPolicy');
+  const sections = t.raw('sections') as PolicySections[]
+
   return (
     <main>
       <Header />
 
       <section className="w-full flex flex-col justify-center items-center">
         <h1 className="w-full py-15 p-5 text-2xl text-center font-sans font-bold lg:text-5xl text-white bg-[#00C950]">
-          POLITYKA PRYWATNOŚCI
+          {t('title')}
         </h1>
-        {TextContent.map((item) => (
+        {sections.map((item, index) => (
           <Paragraph
-            key={item.heading}
+            key={index}
             heading={item.heading}
             content={item.content}
           />
